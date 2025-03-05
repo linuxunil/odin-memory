057302e5ef8dc7cc264488d165b04d087a6ce1a6
Matthew Leonberger <linuxunil@gmail.com>
2025-03-05 12:46:19 +0000

    Removed erronious imports and placeholders. Changed title.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import GameCard from "./components/GameCard";
 import "./App.css";
 import { Navbar, Nav, NavDropdown, Row, Col, Container } from "react-bootstrap";
 import axios from "./axios";
-import { propTypes } from "react-bootstrap/esm/Image";
 
 const defaultPokedex = [
   {
@@ -138,21 +137,6 @@ function App() {
         <Navbar.Brand href="#home">Pokemon Memorization</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          {/* <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav> */}
         </Navbar.Collapse>
           <Navbar.Collapse className="justify-content-end">
             <Col>
