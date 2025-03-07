eb61888375eb3d69a1c18bcee1e3b9653f6f7333
Matthew Leonberger <linuxunil@gmail.com>
2025-03-07 17:05:10 +0000

    PokeDex is populated with more entries and sliced for display.

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,115 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GameCard from "./components/GameCard";
 import "./App.css";
 import { Navbar, Nav, NavDropdown, Row, Col, Container, CardGroup } from "react-bootstrap";
+import axios from "./axios";
 
-const defaultPokedex = [
-  {
-    id: 1,
-    name: "Pikachu",
-    clicked: false,
-  },
-  {
-    id: 2,
-    name: "Charmander",
-    clicked: false,
-  },
-  {
-    id: 3,
-    name: "Squirtle",
-    clicked: false,
-  },
-  {
-    id: 4,
-    name: "Jigglypuff",
-    clicked: false,
-  },
-  {
-    id: 5,
-    name: "Meowth",
-    clicked: false,
-  },
-  {
-    id: 6,
-    name: "Eevee",
-    clicked: false,
-  },
-  {
-    id: 7,
-    name: "Snorlax",
-    clicked: false,
-  },
-  {
-    id: 8,
-    name: "Gengar",
-    clicked: false,
-  },
-  {
-    id: 9,
-    name: "Mewtwo",
-    clicked: false,
-  },
-  {
-    id: 10,
-    name: "Charizard",
-    clicked: false,
-  },
-  {
-    id: 11,
-    name: "Vaporeon",
-    clicked: false,
-  },
-  {
-    id: 12,
-    name: "ditto",
-    clicked: false,
-  },
-];
+// const defaultPokedex = [
+//   {
+//     id: 1,
+//     name: "Pikachu",
+//     clicked: false,
+//   },
+//   {
+//     id: 2,
+//     name: "Charmander",
+//     clicked: false,
+//   },
+//   {
+//     id: 3,
+//     name: "Squirtle",
+//     clicked: false,
+//   },
+//   {
+//     id: 4,
+//     name: "Jigglypuff",
+//     clicked: false,
+//   },
+//   {
+//     id: 5,
+//     name: "Meowth",
+//     clicked: false,
+//   },
+//   {
+//     id: 6,
+//     name: "Eevee",
+//     clicked: false,
+//   },
+//   {
+//     id: 7,
+//     name: "Snorlax",
+//     clicked: false,
+//   },
+//   {
+//     id: 8,
+//     name: "Gengar",
+//     clicked: false,
+//   },
+//   {
+//     id: 9,
+//     name: "Mewtwo",
+//     clicked: false,
+//   },
+//   {
+//     id: 10,
+//     name: "Charizard",
+//     clicked: false,
+//   },
+//   {
+//     id: 11,
+//     name: "Vaporeon",
+//     clicked: false,
+//   },
+//   {
+//     id: 12,
+//     name: "ditto",
+//     clicked: false,
+//   },
+// ];
 
-// FIXME: When high score is recorded images stop changing.
 function App() {
+  const [fetchParams] = useState({ limit: '1000', offset: '0' });
+  const [gameDiff, setDiff] = useState(12);
+  const [gameStart, setStart] = useState(6);
+
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-  const [pokedex,] = useState(defaultPokedex);
+  const [pokeDex, setPokeDex] = useState();
   const [selectedTokens, setSelected] = useState([]);
-  const [pokemonToDisplay, setPokemonToDisply] = useState(defaultPokedex);
+  const [pokemonToDisplay, setPokemonToDisply] = useState();
+
+  useEffect(() => {
+    //Fetch list of pokemon for pokedex
+    async function fetchPokedex() {
+      try {
+        await axios.get(`?limit=${fetchParams.limit}&offset=${fetchParams.offset}`).then((request) => {
+          const newDex = request.data.results.map((pokemon, index) => { return { name: pokemon.name, url: pokemon.url, index } });
+          setPokeDex(newDex);
+          setPokemonToDisply(newDex.slice(gameStart, (gameDiff + gameStart) - 1));
+        });
+      } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.error(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error('Error', error.message);
+        }
+        console.log(error.config);
+      }
+    }
+    fetchPokedex();
+    console.log(pokeDex);
+
+  }, [fetchParams.limit, fetchParams.offset]);
 
   function gameOver(newScore) {
     // Check if score is higher than old HS
@@ -81,12 +118,11 @@ function App() {
       setHighScore(newScore);
     }
     // reset score
-    // TODO: Reset clicked values.
     setScore(0);
   }
 
   function resetGame() {
-    const resetDex = pokedex.map((pokemon) => {
+    const resetDex = pokeDex.map((pokemon) => {
       return { ...pokemon, clicked: false };
     });
     console.log(resetDex);
@@ -144,23 +180,31 @@ function App() {
         </Navbar >
       </Container>
       <Container pb={4} fluid>
-        <Row>
-          {pokemonToDisplay.slice(0, (pokemonToDisplay.length / 2) - 1).map((pokemon, index) => (
-            <GameCard
-              key={index}
-              pokemon={pokemon}
-              handleClick={handleClick}
-            />
-          ))}
-          {pokemonToDisplay.slice((pokemonToDisplay.length / 2), pokemonToDisplay.length - 1).map((pokemon, index) => (
-            <GameCard
-              key={index}
-              pokemon={pokemon}
-              handleClick={handleClick}
-            />
-          ))}
-        </Row>
-      </Container>
+
+        {pokemonToDisplay &&
+          <Row>
+            {
+              pokemonToDisplay.slice(0, (pokemonToDisplay.length / 2) - 1).map((pokemon, index) => (
+                <GameCard
+                  key={index}
+                  tokenName={pokemon.name}
+                  tokenURL={pokemon.url}
+                  handleClick={handleClick}
+                />
+              ))
+            }
+            {pokemonToDisplay.slice((pokemonToDisplay.length / 2), pokemonToDisplay.length - 1).map((pokemon, index) => (
+              <GameCard
+                key={index}
+                tokenName={pokemon.name}
+                tokenURL={pokemon.url}
+                handleClick={handleClick}
+              />
+
+            ))}
+          </Row>
+        }
+      </Container >
     </>
   );
 }
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,50 +1,27 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Image, Col, Placeholder } from "react-bootstrap";
-import axios from "../axios";
 
 //Display picture of pokemon
 //Display pokemon name
 // TODO: Flip cards on click.
 export default function GameCard(props) {
   const [imageURL, setURL] = useState(null); // URL for image from API
-  const [name, setName] = useState(props.pokemon.name); // Name of pokemon
+  const [name, setName] = useState(props.tokenName); // Name of pokemon
 
   useEffect(() => {
     // Get image to display on card
-    async function getImageURL() {
-      // We have to split the response due to 'official-artwork' hyphen
-      let response; // First part of response
-      let sprites; // second part of response
-      try {
-        // get json from api. baseURL in axios.js + pokemon name
-        response = await axios.get(props.pokemon.name);
-        // get first half listing sprites
-        sprites = response.data.sprites.other["official-artwork"];
-        //set url to the default artwork
-        setURL(sprites.front_default);
-        // set the name.
-        setName(props.pokemon.name);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getImageURL();
-  }, [imageURL, props.pokemon.name]);
+    axios.get(props.tokenURL).then((request) =>
+      setURL(request.data.sprites.other['official-artwork'].front_default)).catch((error) =>
+        confirm.error(error));
+    setName(props.tokenName);
+    console.debug(`Set ${props.tokenName} url to ${props.tokenURL}`)
+  }, [props.tokenName, props.tokenURL]);
 
   return (
     <Col xs={4} md={3}>
       <Image src={imageURL} fluid onClick={() => { props.handleClick(name) }} />
       <p>{name}</p>
     </Col>
-    // <Col key={props.index}>
-    //   <Card onClick={() => { props.handleClick(name); }} style={{
-    //     fontSize: "5vw"
-    //   }}>
-    //     <Card.Img variant="top" src={imageURL} />
-    //     <Card.Body>
-    //       <Card.Text>{name}</Card.Text>
-    //     </Card.Body >
-    //   </Card >
-    // </Col>
   );
 }
